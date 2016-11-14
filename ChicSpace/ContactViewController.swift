4dca27be372d76de6ec205a8117ff6c783a2ac6e
KO527 <Kwajo.oseiakoto@gmail.com>
2016-11-14 14:49:27 -0500

    updated ContactViewController.swift

--- ChicSpace/ContactViewController.swift
+++ ChicSpace/ContactViewController.swift
@@ -2,7 +2,7 @@
 //  ContactViewController.swift
 //  ChicSpace
 //
-//  Created by Kwajo Osei-Akoto1 on 10/30/16.
+//  Created by Kwajo Osei-Akoto1 on 11/2/16.
 //  Copyright © 2016 Kwajo Osei-Akoto1. All rights reserved.
 //
 
@@ -10,17 +10,17 @@ import UIKit
 
 class ContactViewController: UIViewController {
 
-
+    
     @IBOutlet weak var scrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.addSubview(scrollView)
-        // Do any additional setup after loading the view.
+        // Do any additional setup after the view
     }
+    
     override func viewWillLayoutSubviews(){
         super.viewWillLayoutSubviews()
-        
-        scrollView.contentSize = CGSize(width: 375, height: 900)
+        scrollView.contentSize = CGSize(width: 375, height: 800)
     }
+
 }
