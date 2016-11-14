7026214137bf5135de7efde3808118e3ef203620
KO527 <Kwajo.oseiakoto@gmail.com>
2016-11-14 14:51:27 -0500

    added Property Class

---
+++ ChicSpace/Property.swift
@@ -0,0 +1,15 @@
+//
+//  Property.swift
+//  ChicSpace
+//
+//  Created by Kwajo Osei-Akoto1 on 11/13/16.
+//  Copyright © 2016 Kwajo Osei-Akoto1. All rights reserved.
+//
+
+import Foundation
+
+class Property {
+    var name: String?
+    var propertyImage: String?
+    var cellImage: String?
+}
